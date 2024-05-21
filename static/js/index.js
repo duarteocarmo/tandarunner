43866edb1b1054c817c83c1f385ad3c11beab694
Duarte OC <me@duarteocarmo.com>
2024-05-21 23:30:49 +0200

    third commit

--- static/js/index.js
+++ static/js/index.js
@@ -14,3 +14,11 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const textareaEle = document.getElementById("textarea");
+  textareaEle.addEventListener("input", () => {
+    textareaEle.style.height = "auto";
+    textareaEle.style.height = `${textareaEle.scrollHeight}px`;
+  });
+});
